1f9d1b36c10f75e83e9fc35c1f568c2e0145cb39
skdyekdtls <skdyekdtls@gmail.com>
2024-04-01 00:03:11 +0900

    커밋

--- Common/d3dApp.cpp
+++ Common/d3dApp.cpp
@@ -454,19 +454,7 @@ bool D3DApp::InitDirect3D()
 	HR(dxgiAdapter->GetParent(__uuidof(IDXGIFactory), (void**)&dxgiFactory));
 
 	HR(dxgiFactory->CreateSwapChain(md3dDevice, &sd, &mSwapChain));
-	//dxgiFactory->MakeWindowAssociation(mhMainWnd, DXGI_MWA_NO_WINDOW_CHANGES);
-	UINT i = 0;
-	IDXGIAdapter* pAdapter;
-	std::vector <IDXGIAdapter*> vAdapters;
-	while (dxgiFactory->EnumAdapters(i, &pAdapter) != DXGI_ERROR_NOT_FOUND)
-	{
-		vAdapters.push_back(pAdapter);
-		++i;
-	}
 
-	std::wstring msg = L"adapter num : ";
-	msg += std::to_wstring(i);
-	MessageBox(mhMainWnd, L"hi", msg.c_str(), MB_OK);
 	ReleaseCOM(dxgiDevice);
 	ReleaseCOM(dxgiAdapter);
 	ReleaseCOM(dxgiFactory);
